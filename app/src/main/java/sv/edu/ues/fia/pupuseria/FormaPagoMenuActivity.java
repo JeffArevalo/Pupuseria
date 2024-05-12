e82ba5893322bebc2493f70699d6253e33859610
vp20007 <vp20007@ues.edu.sv>
2024-05-12 11:07:40 -0600

    Menus Forma Pago, Pedido y Venta

--- app/src/main/java/sv/edu/ues/fia/pupuseria/FormaPagoMenuActivity.java
+++ app/src/main/java/sv/edu/ues/fia/pupuseria/FormaPagoMenuActivity.java
@@ -2,13 +2,38 @@ package sv.edu.ues.fia.pupuseria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class FormaPagoMenuActivity extends AppCompatActivity {
+public class FormaPagoMenuActivity extends ListActivity {
+
+    //Lista del menu forma de pago
+    String[] menu={"Insertar Forma de Pago","Eliminar Forma de Pago","Consultar Forma de Pago", "Actualizar Forma de Pago"};
+    String[] activities={"FormaPagoInsertarActivity","FormaPagoEliminarActivity","FormaPagoConsultarActivity", "FormaPagoActualizarActivity"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_forma_pago_menu);
+
+        ListView listView = getListView();
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, menu);
+        setListAdapter(adapter);
+    }
+
+    protected void onListItemClick(ListView l, View v, int position, long id){
+        super.onListItemClick(l, v, position, id);
+        String nombreValue=activities[position];
+
+        try{
+            Class<?> clase=Class.forName("sv.edu.ues.fia.pupuseria."+nombreValue);
+            Intent inte = new Intent(this,clase);
+            this.startActivity(inte);
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
     }
 }
