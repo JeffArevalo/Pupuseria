49042d58f6d29fd82b4135d046c210bc9f7a55f2
JaimeGD07 <jaimegildiaz3838@gmail.com>
2024-05-31 00:20:19 -0600

    mejoras en crud de tienda

--- app/src/main/java/sv/edu/ues/fia/pupuseria/TiendaEliminarActivity.java
+++ app/src/main/java/sv/edu/ues/fia/pupuseria/TiendaEliminarActivity.java
@@ -25,15 +25,24 @@ public class TiendaEliminarActivity extends Activity {
     }
 
     public void eliminarTienda(View v) {
-        String regEliminados;
-        Tienda tiendaDlt = new Tienda();
-        tiendaDlt.setId_direccion(Integer.parseInt(editIdDir.getText().toString()));
-        tiendaDlt.setId_tienda(Integer.parseInt(editIdTienda.getText().toString()));
+        try{
+            if(editIdDir.getText().toString().isEmpty() && editIdTienda.getText().toString().isEmpty()){
+                Toast.makeText(this, "Los campos ID's se esta enviando vacio, por favor completar", Toast.LENGTH_SHORT).show();
+            }else{
+                String regEliminados;
+                Tienda tiendaDlt = new Tienda();
+                tiendaDlt.setId_direccion(Integer.parseInt(editIdDir.getText().toString()));
+                tiendaDlt.setId_tienda(Integer.parseInt(editIdTienda.getText().toString()));
 
-        helper.abrir();
-        regEliminados= helper.eliminarTienda(tiendaDlt);
-        helper.cerrar();
+                helper.abrir();
+                regEliminados= helper.eliminarTienda(tiendaDlt);
+                helper.cerrar();
 
-        Toast.makeText(this, regEliminados, Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, regEliminados, Toast.LENGTH_SHORT).show();
+            }
+        }catch (Exception e){
+            Toast.makeText(this, "A ocurrido un error durante la ejecucion en Eliminar Tienda", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
