2ff8a15fea226cd3914dd018eda5733fa9c2c810
vp20007 <vp20007@ues.edu.sv>
2024-06-02 15:28:37 -0600

    Menu de servicios

---
+++ app/src/main/java/sv/edu/ues/fia/pupuseria/ServicioInsertarFormaPagoActivity.java
@@ -0,0 +1,14 @@
+package sv.edu.ues.fia.pupuseria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ServicioInsertarFormaPagoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_servicio_insertar_forma_pago);
+    }
+}
