61938abed4ec72d284922872d41a9c743ab292df
JaimeGD07 <jaimegildiaz3838@gmail.com>
2024-06-02 13:23:55 -0600

    pantallas de WS Admin

--- app/src/main/java/sv/edu/ues/fia/pupuseria/AdministradorWebServiceActivity.java
+++ app/src/main/java/sv/edu/ues/fia/pupuseria/AdministradorWebServiceActivity.java
@@ -10,11 +10,10 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class AdministradorMenuActivity extends ListActivity {
+public class AdministradorWebServiceActivity extends ListActivity {
 
-    String[] menu ={"Insertar", "Actualizar", "Consultar", "Eliminar", "Servicios Web"};
-    String[] activities={"AdministradorInsertarActivity", "AdministradorActualizarActivity",
-            "AdministradorConsultarActivity", "AdministradorEliminarActivity", "AdministradorWebServiceActivity"};
+    String[] menu ={"Insertar", "Consultar"};
+    String[] activities={"AdministradorInsertarWSActivity", "AdministradorConsultarWSActivity"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +26,7 @@ public class AdministradorMenuActivity extends ListActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menu);
         setListAdapter(adapter);
     }
+
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id){
         super.onListItemClick(l,v,position,id);
